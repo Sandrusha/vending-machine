a65083b26cac4263075b9cfd1ead93360b6f7596
Sandra P <putere.sandra@gmail.com>
2019-12-22 16:03:30 +0200

    final project

--- main.js
+++ main.js
@@ -98,6 +98,10 @@ function populate() {
         var productsRow = document.createElement("div");
         for (var j = 0; j < columns; j++) {
             var product = document.createElement("div");
+            //stoc pt fiecare produs (data-atribute)
+            product.setAttribute("data-stock", productsPerSlot);
+            //setam o clasa pentru fiecare produs care sa fie codul produsului
+            product.classList.add("cls-" + products[j+columns*i].code);
             console.log(j+columns*i); //liniarizarea matricei - indici cu valori de la 0 la 14
 
             var productCode = document.createElement("div")
@@ -118,30 +122,38 @@ function populate() {
         }
         divContainer.appendChild(productsRow);
     }
-
-    //trebuie sa verificam daca avem suficient de multe produse ca sa populam automatul
-    //stoc pt fiecare produs (data-atribute)
-    //setam o clasa pentru fiecare produs care sa fie codul produsului
-    //data-atribute pentru fiecare produs care sa reprezinte indexul din lista
 }
 
 function listenBtns() {
     var okBtn = document.getElementById("okBtn");
     okBtn.addEventListener("click", showSum);
+
+    var changeBtn = document.getElementById("changeBtn");
+    changeBtn.addEventListener("click", giveChange);
 }
 
 function showSum() {
     var sumNumber = document.getElementById("sumNumber");
     var sum = document.getElementById("sum");
-    sum.innerHTML = sumNumber.value;
+    sum.innerHTML = parseInt(sumNumber.value) + parseInt(sum.innerHTML);
     sumNumber.value = 0;
+}
+
+function giveChange() {
+    var sum = document.getElementById("sum");
+    var errors = document.getElementById("errs");
+    errors.innerHTML = "Your change is: " + sum.innerHTML;
+    sum.innerHTML = 0;
+    delErrs();
 }
 
 function listenKeyPad() {
     var productCode = document.getElementById("productCode");
     var eraseBtn = document.getElementById("eraseBtn");
+    var okBtn = document.getElementById("codOk");
 
     eraseBtn.addEventListener("click", deleteCode);
+    okBtn.addEventListener("click", returnProduct);
 
     var keyPad = document.getElementById("keyPad");
     keyPad.addEventListener("click", onNumberPress);
@@ -154,24 +166,60 @@ function deleteCode() {
 
 function onNumberPress(event) {
     var productCode = document.getElementById("productCode");
-    var key = event.target.closest("div.number"); // luam cel mai apropiat div cu clasa "number" de event.target
-    if (productCode.innerHTML.length === 2) {
-        //da produsul
-        returnProduct();
-        return;
-    } else {
-        if (key.classList.contains("number")) {
-            productCode.innerHTML += key.innerHTML;  
-        }
+    var key = event.target.closest("div.number span"); // luam cel mai apropiat div cu clasa "number" de event.target
+    if (key != null) {
+        productCode.innerHTML += key.innerHTML;
     }
 }
 
 function returnProduct() {
     //verificam daca exista codul
-    //verificam daca produsul are stoc
-    //verificam daca avem bani
-    //stoc --
-    //bani --
-    //stoc = zero--> schimbam imaginea
-    //de scos imaginea de pe buton cand returnam produsul si de afisat in ecran codul
+    var productCode = document.getElementById("productCode").innerHTML;
+    var errors = document.getElementById("errs");
+    var productFound = false;
+    for (i = 0; i < products.length; i++) {
+        if (productCode == products[i].code) { //produsul exista
+            var htmlProduct = document.getElementsByClassName("cls-" + productCode)[0];
+            //verificam daca produsul are stoc
+            var productStock = parseInt(htmlProduct.getAttribute("data-stock")); //productStock = integer
+            if (productStock > 0 ) {
+                //verificam daca avem bani
+                var sum = document.getElementById("sum");
+                var initSum = parseInt(sum.innerHTML);
+                if (initSum >= products[i].price) {
+                    productStock --;
+                    htmlProduct.setAttribute("data-stock", productStock);
+                    //stoc = zero--> schimbam imaginea
+                    if (productStock === 0) {
+                        var productImg = htmlProduct.getElementsByTagName("img")[0];
+                        productImg.setAttribute("src", "media/products/out-of-stock.png");
+                    }
+                    sum.innerHTML = initSum - products[i].price;
+                    document.getElementById("productCode").innerHTML = "";
+                } else {
+                    errors.innerHTML = "You don't have enough money!!!";
+                    delErrs();
+                }
+            } else {
+                errors.innerHTML = "Out Of Stock!!!";
+                delErrs();
+            }
+            productFound = true;
+        }
+    } 
+    if (productFound === false) {
+        errors.innerHTML = "Product Not Found!!!";
+        document.getElementById("productCode").innerHTML = "";
+        delErrs();
+    }     
 }
+
+function delErrs() {
+    var productCode = document.getElementById("productCode");
+    productCode.innerHTML = "";
+    setTimeout(function(){
+        var errors = document.getElementById("errs");
+        errors.innerHTML="";
+    }, 5000);
+}
+
---
+++ media/README.md
@@ -0,0 +1,19 @@
+Hi guys and welcome to my Vending Machine project!
+
+First of all, this project is made in vanilla JS. I started learning React from our interview, but I didn't have enough time to do the React application. I sincerely tell you that in the next period I will also make this Vending Machine as a React app :) 
+
+Secondly, I want to let you know how my Vending Machine works (PC only ¯\_(ツ)_/¯).
+You need to follow those steps:
+1. Open https://sandrusha.github.io/vending-machine/;
+2. Enter the amount of money you have for getting some awesome products from the Vendomat;
+3. Press OK if / when the amount is the right one;
+4. Now you can pressing the numbers from the keypad above the Sum section, along with OK or DEL (if you want to delete the code number);
+5. If you need to take your change, please press Change button and the Vending Machine will return your change.
+
+Some addition info:
+5. You can see under Dev Tool the Stock of the Products;
+6. If you are getting the same products until Stock = 0, you can see the unavailablity of that product by changing the image with one of Out Of Stock and also, an error will show up;
+7. If you try to get a product which does not exist, you will get an error instead;
+8. If the Product costs more then your available amount, you will also get an error.
+
+Thank you for your attention!
